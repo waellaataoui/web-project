afcbeffc208d1f92962671ca3161a2585d420945
ha50221302 <hamida.tlili@issatso.u-sousse.tn>
2021-05-23 11:09:11 +0200

    Testquiz

---
+++ frontend/src/components/Welcome/index.js
@@ -0,0 +1,22 @@
+import React, { useState, useContext, useEffect } from 'react'
+import Quiz from '../Quiz'
+import Loader from '../Loader/'
+
+const Welcome = props => {
+
+    return  (
+        <div>
+        <Loader 
+            loadingMsg={"Authentification ..."}
+            styling={{textAlign: 'center', color: '#FFFFFF'}}
+        />
+        <div className="quiz-bg">
+            <div className="container">
+                <Quiz />
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Welcome
